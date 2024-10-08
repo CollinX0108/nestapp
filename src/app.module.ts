f5ac3d77457304342fbe15b33e8921eb8967980e
CollinX0108 <collin0108@hotmail.com>
2024-10-08 17:32:35 -0500

    Guardando cambios locales antes de revert

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ReservasModule } from './reservas/reservas.module';
 import { AuthModule } from './auth/auth.module';
-import { EquiposModule } from './equipos/equipos.module';
-import { config } from 'process';
 
 @Module({
   imports: [
@@ -14,27 +12,23 @@ import { config } from 'process';
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        const isProduction = configService.get('NODE_ENV') === 'production';
-        return {
-          type: 'postgres',
-          url: configService.get('DATABASE_URL'), // Usar DATABASE_URL si está disponible
-          host: configService.get('DB_HOST'),
-          port: +configService.get<number>('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_NAME'),
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize: !isProduction, // Desactivado en producción
-          ssl: isProduction ? { rejectUnauthorized: false } : false, // SSL solo en producción
-        };
-      },
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Cuidado en producción
+      }),
       inject: [ConfigService],
     }),
     UsersModule,
+    ReservasModule,
     AuthModule,
     ReservasModule,
-    EquiposModule,
+    // Otros módulos...
   ],
 })
 export class AppModule {}
