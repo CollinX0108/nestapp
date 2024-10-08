4014ac3cfa4d8f3060a42ecab5a7ce04e8a1f7f7
CollinX0108 <collin0108@hotmail.com>
2024-10-08 16:12:47 -0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
